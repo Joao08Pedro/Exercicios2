ab91ea3def6bc00ab0ab539965a6ed1da630c4af
Joao08Pedro <joaopedro120908@gmail.com>
2025-03-07 17:25:23 -0300

    prepronto

--- contagem.js
+++ contagem.js
@@ -1,1 +1,6 @@
+const n = Math.floor(Math.random() * 10) + 1
 
+for (let i = 0; i <= n; i++) {
+    console.log(i);
+    
+}
---
+++ olavariasvezes.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")();
+const N = +prompt ("Quantas vezes você quer um olá?")
+
+
+for (let i = 1; i <= N; i++) {
+    const i = "Olá!"
+    console.log(i);
+    
+}
